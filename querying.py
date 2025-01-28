51656e55732db156e0252953435bca68ad188309
Antaldeurloo <antal13deurloo@gmail.com>
2025-01-28 01:10:24 +0100

    running

--- querying.py
+++ querying.py
@@ -3,7 +3,7 @@ import pandas as pd
 #from ci_group.revolve2.ci_group.genotypes.cppnwin._body_genotype_v2_multineat_genotype_pickle_wrapper import MultineatGenotypePickleWrapper
 
 # Connect to the SQLite database
-conn = sqlite3.connect('adv_30_run2.sqlite.sqlite')
+conn = sqlite3.connect('final_std_cross.sqlite')
 cursor = conn.cursor()
 
 # Run a query
@@ -37,7 +37,7 @@ query3 = """
 
 """
 
-cursor.execute(query3)
+cursor.execute(query)
 
 # Fetch all rows and column names
 rows = cursor.fetchall()
